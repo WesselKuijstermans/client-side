5bd08d899ca976cce1a41f0d3e7569b8c5e01fa5
Wessel Kuijstermans <w.kuijstermans@home.nl>
2025-04-10 15:47:36 +0200

    Removed unnecessary logs

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -9,15 +9,6 @@ import { AppModule } from './app/app.module';
 import * as pg from 'pg-connection-string';
 
 async function bootstrap() {
-  Logger.log('====================================a;ljszdfhgflkzhjsdgblfkhjasgbdlfgvhjkavbsdkljhvb');
-  const dbConfig = pg.parse(process.env.DB_STRING);
-  Logger.log('DB_HOST:', dbConfig.host || '❌ UNDEFINED ❌');
-  Logger.log('DB_HOST:', dbConfig.port || '❌ UNDEFINED ❌');
-  Logger.log('DB_HOST:', dbConfig.user || '❌ UNDEFINED ❌');
-  Logger.log('DB_HOST:', dbConfig.password || '❌ UNDEFINED ❌');
-  Logger.log('DB_HOST:', dbConfig.database || '❌ UNDEFINED ❌');
-  Logger.log('====================================sz.kfjbglakshdbfvlkjzsdfldkfvbawsdljkhfvbas');
-
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://polite-water-03a063e03.6.azurestaticapps.net/', 'http://localhost:4200'],	
