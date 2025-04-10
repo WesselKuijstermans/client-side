cb59aa9278fcad9683aa44faa60ff4a54b963843
Wessel Kuijstermans <w.kuijstermans@home.nl>
2025-04-10 13:59:45 +0200

    Log db string

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -6,10 +6,16 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import * as pg from 'pg-connection-string';
 
 async function bootstrap() {
   Logger.log('====================================a;ljszdfhgflkzhjsdgblfkhjasgbdlfgvhjkavbsdkljhvb');
-  Logger.log('DB_STRING:', process.env.DB_STRING || '❌ UNDEFINED ❌');
+  const dbConfig = pg.parse(process.env.DB_STRING);
+  Logger.log('DB_HOST:', dbConfig.host || '❌ UNDEFINED ❌');
+  Logger.log('DB_HOST:', dbConfig.port || '❌ UNDEFINED ❌');
+  Logger.log('DB_HOST:', dbConfig.user || '❌ UNDEFINED ❌');
+  Logger.log('DB_HOST:', dbConfig.password || '❌ UNDEFINED ❌');
+  Logger.log('DB_HOST:', dbConfig.database || '❌ UNDEFINED ❌');
   Logger.log('====================================sz.kfjbglakshdbfvlkjzsdfldkfvbawsdljkhfvbas');
 
   const app = await NestFactory.create(AppModule);
